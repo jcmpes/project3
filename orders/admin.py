1853ca35bf95bd724c671608786c9668236dcfcb
JC <jcmpes@gmail.com>
2020-06-13 11:14:50 +0200

    Create models and add items

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import RegularPizza, SicilianPizza, Topping, Sub, Pasta, DinnerPlatter, Salad
+
 # Register your models here.
+admin.site.register(RegularPizza)
+admin.site.register(SicilianPizza)
+admin.site.register(Topping)
+admin.site.register(Sub)
+admin.site.register(Pasta)
+admin.site.register(DinnerPlatter)
+admin.site.register(Salad)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class RegularPizza(models.Model):
+	name = models.CharField(max_length=32)
+	small = models.DecimalField(max_digits=4, decimal_places=2)
+	large = models.DecimalField(max_digits=4, decimal_places=2)
+
+
+class SicilianPizza(models.Model):
+	name = models.CharField(max_length=32)
+	small = models.DecimalField(max_digits=4, decimal_places=2)
+	large = models.DecimalField(max_digits=4, decimal_places=2)
+
+
+class Topping(models.Model):
+	name = models.CharField(max_length=32)
+
+
+class Sub(models.Model):
+	name = models.CharField(max_length=32)
+	small = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+	large = models.DecimalField(max_digits=4, decimal_places=2)
+
+
+class Pasta(models.Model):
+	name = models.CharField(max_length=32)
+	price = models.DecimalField(max_digits=4, decimal_places=2)
+
+
+class DinnerPlatter(models.Model):
+	name = models.CharField(max_length=32)
+	small = models.DecimalField(max_digits=4, decimal_places=2)
+	large = models.DecimalField(max_digits=4, decimal_places=2)
+
+class Salad(models.Model):
+	name = models.CharField(max_length=32)
+	price = models.DecimalField(max_digits=4, decimal_places=2)
