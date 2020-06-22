401442fe078948f9aefd824b6806d1815cddc898
JC <jcmpes@gmail.com>
2020-06-22 19:16:38 +0200

    Removed unused models

--- orders/models.py
+++ orders/models.py
@@ -19,50 +19,6 @@ LABEL_CHOICES = (
 	('D', 'danger')	
 )
 
-# Create your models here.
-# class RegularPizza(models.Model):
-# 	name = models.CharField(max_length=32)
-# 	small = models.DecimalField(max_digits=4, decimal_places=2)
-# 	large = models.DecimalField(max_digits=4, decimal_places=2)
-
-# 	def __str__(self):
-# 		return self.name
-
-
-# class SicilianPizza(models.Model):
-# 	name = models.CharField(max_length=32)
-# 	small = models.DecimalField(max_digits=4, decimal_places=2)
-# 	large = models.DecimalField(max_digits=4, decimal_places=2)
-
-
-# class Topping(models.Model):
-# 	name = models.CharField(max_length=32)
-
-
-# class Sub(models.Model):
-# 	name = models.CharField(max_length=32)
-# 	small = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
-# 	large = models.DecimalField(max_digits=4, decimal_places=2)
-
-
-# class Pasta(models.Model):
-# 	name = models.CharField(max_length=32)
-# 	price = models.DecimalField(max_digits=4, decimal_places=2)
-
-# 	def __str__(self):
-# 		return self.id + ' - ' + self.name + ' is $' + self.price
-
-
-# class DinnerPlatter(models.Model):
-# 	name = models.CharField(max_length=32)
-# 	small = models.DecimalField(max_digits=4, decimal_places=2)
-# 	large = models.DecimalField(max_digits=4, decimal_places=2)
-
-# class Salad(models.Model):
-# 	name = models.CharField(max_length=32)
-# 	price = models.DecimalField(max_digits=4, decimal_places=2)
-
-
 
 class Item(models.Model):
 	title = models.CharField(max_length=100)
